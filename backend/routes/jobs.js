8b7fe5ebc15f46f2b8935bb200d5355e3e3ba636
Rishabh Singhvi <71822131+rishabhRsinghvi@users.noreply.github.com>
2024-08-27 11:06:20 +0530

    Update jobs.js

--- backend/routes/jobs.js
+++ backend/routes/jobs.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const fetch = require('node-fetch');
 
-// Job Search Endpoint
 router.get('/', async (req, res) => {
   const { query, location } = req.query;
   const apiUrl = `https://jsearch.p.rapidapi.com/search?query=${query}&location=${location}`;
