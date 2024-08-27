695ab2a8ee459d4750424cf66d1ce80e08ae39eb
Rishabh Singhvi <71822131+rishabhRsinghvi@users.noreply.github.com>
2024-08-27 11:05:31 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,15 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-// Routes
 const jobsRoute = require('./routes/jobs');
 const submitJobRoute = require('./routes/submitJob');
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Use Routes
 app.use('/jobs', jobsRoute);
 app.use('/submit-job', submitJobRoute);
 
-// Listen to port 5000
 app.listen(5000, () => {
   console.log('Server running on port 5000');
 });
